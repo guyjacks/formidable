acc5346a18902a693f24b7cefb37ceb3c6282c5b
Guy Jacks <guy.jacks@fndrz.com>
2015-04-04 20:25:24 -0400

    implemented package structure and wrote setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Formidable',
+    version='0.1.0-dev',
+    packages=['formidable', ],
+    license='The MIT License (MIT)',
+    long_description=open('README.txt').read(),
+)
