a01f62daa06b74a9ab5499e85afbb6f7b282e6db
Guy Jacks <guy.jacks@fndrz.com>
2015-04-05 18:25:16 -0400

    more structure

---
+++ README.txt
@@ -0,0 +1,17 @@
+==========
+Formidable
+==========
+
+Formidable handles webforms.
+
+Ajax form submission
+====================
+
+code example goes here
+
+Standard Post Sumbission
+========================
+
+code example goes here
+
+note - this is REst markdown which is readable by pypi.
--- setup.py
+++ setup.py
@@ -3,7 +3,10 @@ from distutils.core import setup
 setup(
     name='Formidable',
     version='0.1.0-dev',
+    author='Guy Jacks III',
+    author_email='guy.jacks@gmail.com',
     packages=['formidable', ],
     license='The MIT License (MIT)',
-    long_description=open('README.md').read(),
+    description='Form handler for python',
+    long_description=open('README.txt').read()
 )
